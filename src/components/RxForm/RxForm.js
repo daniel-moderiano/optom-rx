77c24bbfb7c83306dc1247662f0b473f12f6ec0a
Daniel <daniel.moderiano@outlook.com>
2021-10-26 16:10:42 +1030

    Add Rxform skeleton for all fieldsets with comment guidelines

--- src/components/RxForm/RxForm.js
+++ src/components/RxForm/RxForm.js
@@ -8,19 +8,42 @@ const RxForm = () => {
   });
 
   const handleChange = (event) => {
-    setData({ ...data, [event.target.name]: event.target.value });
+    setData((prevData) => {
+      return { ...prevData, [event.target.name]: event.target.value }
+    });
   }
 
   return (
     <form className="rxform">
-      <FormField 
-        fieldType="text" 
-        name="drug" 
-        label="Medication" 
-        placeholder={"Enter medication name"} 
-        value={data.drug} 
-        onChange={handleChange} 
-      />
+      {/* Single input to select the medication */}
+      {/* Note there must be enough info to identify the medicine, including form and strength */}
+      <fieldset className="drug">
+        <FormField 
+          fieldType="text" 
+          name="drug" 
+          label="Medication" 
+          placeholder={"Enter medication name"} 
+          value={data.drug} 
+          onChange={handleChange} 
+        />
+      </fieldset>
+
+      {/* Enter the patient Rx details */}
+      <fieldset className="patient">
+        {/* Legal requirements include only the patient's name and address */}
+        {/* Patient Medicare number is however required for ALL PBS Rx, and should be included in general so that the patient may claim under PBS where this price is cheaper. All Aus are valid private prescriptions however. */}
+      </fieldset>
+
+      {/* Enter the provider details */}
+      <fieldset className="provider">
+        {/* Legal requirements include the prescriber's name, address, and contact details, and prescriber num
+        You may also give them the option of adding qualifications */}
+      </fieldset>
+
+      {/* Enter remaining details such as dosage, quantity, and repeats, plus PBS. This section might include the PBS authority code field */}
+      <fieldset className="parameters">
+        {/* Must include quantity and repeats to meet requirements */}
+      </fieldset>
     </form>
   )
 }
