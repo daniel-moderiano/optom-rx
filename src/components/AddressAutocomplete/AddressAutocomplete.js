3b529e0f6ce7f499d1125dff8d44a747f0a649fa
Daniel <daniel.moderiano@outlook.com>
2021-10-27 18:19:46 +1030

    Create custom autocomplete hook

--- src/components/AddressAutocomplete/AddressAutocomplete.js
+++ src/components/AddressAutocomplete/AddressAutocomplete.js
@@ -1,11 +1,11 @@
 /*global google*/ // Used to ignore the breaking 'google isn't defined' error
 // Additional API resources to consider include Bing, HERE, ArcGIS
 import FormField from "../FormField/FormField";
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useRef } from "react"
+import useAutocomplete from "../utils/useAutocomplete";
 
 const AddressAutocomplete = ({ data, setData, handleChange }) => {
-
-  
+  const input = useRef();
 
   const fillAddress = useCallback((placeDetails) => {
     let address = {
@@ -56,38 +56,41 @@ const AddressAutocomplete = ({ data, setData, handleChange }) => {
       ...address,
     }));
 
-  }, [])
+  }, [setData])
 
-  useEffect(() => {
-    const googleScript = document.createElement('script');
+  // useEffect(() => {
+    // const googleScript = document.createElement('script');
 
-    // This process.env system for hiding an API key is COMPLETELY INSECURE for a deployed build. This is purely to hide on Github. In the future, this should be secured on backend
-    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places`;
-    googleScript.async = true;
+  //   // This process.env system for hiding an API key is COMPLETELY INSECURE for a deployed build. This is purely to hide on Github. In the future, this should be secured on backend
+  //   googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places`;
+  //   googleScript.async = true;
 
-    window.document.body.appendChild(googleScript);
+  //   window.document.body.appendChild(googleScript);
 
-    googleScript.addEventListener('load', () => {
-      console.log('script loaded');
-      let input = document.querySelector('#autocomplete');
-      let autocomplete = new google.maps.places.Autocomplete(input, {
-        componentRestrictions: { 'country': ['AU'] },
-        fields: ['address_components'],
-      });
+  //   googleScript.addEventListener('load', () => {
+  //     let input = document.querySelector('#autocomplete');
+  //     let autocomplete = new google.maps.places.Autocomplete(input, {
+  //       componentRestrictions: { 'country': ['AU'] },
+  //       fields: ['address_components', 'name', 'formatted_address', 'adr_address'],
+  //     });
+  //     console.log('in func');
+  //     autocomplete.addListener('place_changed', () => {
+  //       let place = autocomplete.getPlace();
+  //       console.log(place);
+  //       fillAddress(place);
+  //       input.value = "";
+  //       console.log('address filled');
+  //     });
+  //     return autocomplete;
+  //   });
 
-      autocomplete.addListener('place_changed', () => {
-        let place = autocomplete.getPlace();
-        fillAddress(place);
-        console.log('address filled');
-      });
-    });
-
-  }, [fillAddress])
+  // }, [fillAddress])
+  useAutocomplete(input);
 
   return (
     <fieldset>
       <label htmlFor="autocomplete">Address</label>
-      <input type="text" id="autocomplete"/>
+      <input type="text" id="autocomplete" ref={input}/>
 
       <FormField 
         fieldType="text" 
