8807cd6f690429cc82f291dd98fc61ab5ba54436
Daniel <daniel.moderiano@outlook.com>
2021-11-02 20:17:35 +1030

    Modify data to nested dictionary and use spread operator equivalent to add new indication field + all old fields to all drug objects

--- pbs.py
+++ pbs.py
@@ -1,34 +1,36 @@
 # PBS fields in drug.txt
-# data = {
-#   'program-code': '',
-#   'atc-level-code': '',
-#   'atc-type': '',
-#   'atc-print-option': '',
-#   'item-code': '',
-#   'restriction-flag': '',
-#   'has-caution': '',
-#   'has-note': '',
-#   'mq': '',    # maximum quantity
-#   'repeats': '',
-#   'manufacturer-code': '',
-#   'pack-size': '',
-#   'markup-band': '',
-#   'fee-code': '',
-#   'dangerous-drug-code': '',
-#   'brand-premium': '',
-#   'therapeutic-premium': '',
-#   'cp2p': '',    # commonwealth price to pharmacy
-#   'cdpmq': '',   # commonwealth dispensed price for maximum quantity
-#   'lp2p': '',    # lowest price to pharmacist
-#   'ldpmq': '',   # lowest dispensed price for maximum quantity
-#   'mp2p': '',    # manufacturer price to pharmacist
-#   'mdpmq': '',   # manufacturer dispensed price for maximum quantity
-#   'mrvsn': '',   # maximum recordable value for safety net
-#   'bioequivalence': '',
-#   'brand-name': '',
-#   'mp-pt': '',   # MP preferred term
-#   'tpuu-or-mpp-pt': '',    #TPUU or MPP preferred term
-# }
+all_fields = {
+  'program-code': '',
+  'atc-level-code': '',
+  'atc-type': '',
+  'atc-print-option': '',
+  'item-code': '',
+  'restriction-flag': '',
+  'has-caution': '',
+  'has-note': '',
+  'mq': '',    # maximum quantity
+  'repeats': '',
+  'manufacturer-code': '',
+  'pack-size': '',
+  'markup-band': '',
+  'fee-code': '',
+  'dangerous-drug-code': '',
+  'brand-premium': '',
+  'therapeutic-premium': '',
+  'cp2p': '',    # commonwealth price to pharmacy
+  'cdpmq': '',   # commonwealth dispensed price for maximum quantity
+  'lp2p': '',    # lowest price to pharmacist
+  'ldpmq': '',   # lowest dispensed price for maximum quantity
+  'mp2p': '',    # manufacturer price to pharmacist
+  'mdpmq': '',   # manufacturer dispensed price for maximum quantity
+  'mrvsn': '',   # maximum recordable value for safety net
+  'bioequivalence': '',
+  'brand-name': '',
+  'mp-pt': '',   # MP preferred term
+  'tpuu-or-mpp-pt': '',    #TPUU or MPP preferred term
+  'indication-id': '',    # Used to find the indications of a restricted drug
+}
+
 
 # Anatomical Therapeutic Chemical (ATC) classification index is used to classify drugs based on area of intended use. 
 # The ATC level code is a combination of flags that categorise a drug, e.g.
@@ -42,6 +44,7 @@ pres_fields = [
 ]
 # This can be searched using the 'O' prescriber type, and then grabbing the item-codes to then look up more details in drug.txt and restrictions.txt
 
+drugs = {}
 data = []
 
 # Isolate the optometry specific medications (by item code) from the PBS list
@@ -58,35 +61,41 @@ with open(pres_path) as f:
     line_arr = line.split()
     if line_arr[-1] == 'O':
       item_codes.add(line_arr[-2])
-
-print(item_codes)
+      drugs[line_arr[-2]] = {}
 
 # Using the item codes obtained, search the drug.txt doc for pricing and quantity info
 drug_path = 'C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/drug_20211101.txt'
 with open(drug_path) as f:
-  drug = {}
   drug_lines = f.readlines()
   columns = drug_lines[0].strip().split('!')
   for line in drug_lines:
     # Drug.txt uses ! markers as delimmiters, and the fields are listed above in the drug fields array. Not all of these are important information in the context of this app, but all will be extracted for now
     line_arr = line.strip().split('!')
     if line_arr[4] in item_codes:
+      # Must reset drug variable
+      drug = { **all_fields }
       for i in range(len(columns)):
         # Add all data columns under the respective names for every individual medication/drug
         drug[columns[i]] = line_arr[i]
-      data.append(drug)
+      drugs[line_arr[4]] = drug
+
+
+# Using the item code, find the indication IDs for the restrictions on the meds. Note that only authorised medications will have indication IDs
+table_path = 'C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/2021-11-01-v3extracts/LinkExtract_20211101.txt'
+with open(table_path) as f:
+  table_lines = f.readlines()
+  columns = table_lines[0].strip().split('!')
+  for line in table_lines:
+    # Drug.txt uses ! markers as delimmiters, and the fields are listed above in the drug fields array. Not all of these are important information in the context of this app, but all will be extracted for now
+    line_arr = line.strip().split('\t')
+    if line_arr[0] in item_codes:
       # Must reset drug variable
-      drug = {}
-
-for drug in data:
-  print(drug['brand-name'])
-
-# print(data[1]['item-code'])
-# Write mode (switch the 'w' to an 'a' for append mode)\
-# counter = 0
-# with open('C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/pbs-format.txt', 'w') as f:
-#   for field in pbs.split('!'):
-#     print(fields[counter], '\t', field, file=f)
-#     counter += 1
-
-
+      # drug = {}
+      # for i in range(len(columns)):
+      #   # Add all data columns under the respective names for every individual medication/drug
+      #   drug[columns[i]] = line_arr[i]
+      # data.append(drug)
+      drugs[line_arr[0]]['indication-id'] = line_arr[1]
+      
+for code in drugs.keys():
+  print(drugs[code]['brand-name'], drugs[code]['restriction-flag'], drugs[code]['indication-id'])
