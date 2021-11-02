30743ffaa57d195176e4ac87017fd03a71c2003a
Daniel <daniel.moderiano@outlook.com>
2021-11-02 16:16:50 +1030

    Convert item_codes to set instead of list, to allow easier searching

--- pbs.py
+++ pbs.py
@@ -1,18 +1,18 @@
 # PBS fields in drug.txt
-fields = [
-  'program-code',
-  'atc-level-code',
-  'atc-type',
-  'atc-print-option',
-  'item-code',
-  'restriction-flag',
-  'has-caution',
-  'has-note',
-  'mq',    # maximum quantity
-  'repeats',
-  'manufacturer-code',
-  'pack-size',
-  'markup-band',
+data = {
+  'program-code': '',
+  'atc-level-code': '',
+  'atc-type': '',
+  'atc-print-option': '',
+  'item-code': '',
+  'restriction-flag': '',
+  'has-caution': '',
+  'has-note': '',
+  'mq': '',    # maximum quantity
+  'repeats': '',
+  'manufacturer-code': '',
+  'pack-size': '',
+  'markup-band': '',
   'fee-code',
   'dangerous-drug-code',
   'brand-premium',
@@ -28,7 +28,7 @@ fields = [
   'brand-name',
   'mp-pt',   # MP preferred term
   'tpuu-or-mpp-pt'    #TPUU or MPP preferred term
-]
+}
 
 # Anatomical Therapeutic Chemical (ATC) classification index is used to classify drugs based on area of intended use. 
 # The ATC level code is a combination of flags that categorise a drug, e.g.
@@ -42,19 +42,34 @@ pres_fields = [
 ]
 # This can be searched using the 'O' prescriber type, and then grabbing the item-codes to then look up more details in drug.txt and restrictions.txt
 
-item_codes = []
 
-# Isolate the optometry specific medications from the PBS list
-pbs_path = 'C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/Prescriber_type_20211101.txt'
-with open(pbs_path) as f:
-  pbs_lines = f.readlines()
-  for line in pbs_lines:
+
+# Isolate the optometry specific medications (by item code) from the PBS list
+# For reference, at the time of writing, there are 64 approved optometry PBS medications
+item_codes = set()
+# item_set = set(item_codes)
+
+# Modify path as necessary, e.g. each new month with new release of PBS data
+pres_path = 'C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/Prescriber_type_20211101.txt'
+with open(pres_path) as f:
+  pres_lines = f.readlines()
+  for line in pres_lines:
     # The PBS text file in question is always of the format "drug-name item-code prescriber-type"; any changes to thios format should be made visible on dev.pbs.gov.au Hnce we can search by prescriber type, and extract item code
     line_arr = line.split()
     if line_arr[-1] == 'O':
-      item_codes.append(line_arr[-2])
+      item_codes.add(line_arr[-2])
 
 print(item_codes)
+
+# Using the item codes obtained, search the drug.txt doc for pricing and quantity info
+drug_path = 'C:/Users/danie/Documents/Programming/work-projects/optom-rx/info/drug_20211101.txt'
+with open(drug_path) as f:
+  drug_lines = f.readlines()
+  for line in drug_lines:
+    # Drug.txt uses ! markers as delimmiters, and the fields are listed above in the drug fields array. Not all of these are important information in the context of this app, but all will be extracted for now
+    line_arr = line.split('!')
+    if line_arr[4] in item_codes:
+      print(f'item-code: {line_arr[4]}')
 
 # Write mode (switch the 'w' to an 'a' for append mode)\
 # counter = 0
