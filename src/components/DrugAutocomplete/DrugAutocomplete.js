b84dd58e7eb472198e1cd3ba37060f3b16400990
Daniel <daniel.moderiano@outlook.com>
2021-11-03 20:46:09 +1030

    Modify drug autocomplete to handle brand names array

--- src/components/DrugAutocomplete/DrugAutocomplete.js
+++ src/components/DrugAutocomplete/DrugAutocomplete.js
@@ -5,9 +5,10 @@ const DrugAutocomplete = () => {
   const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
+
     let matches = data.filter((drug) => {
       const regex = new RegExp(`^${searchText}`, 'i');
-      return drug['brand-name'].match(regex) || drug['tpuu-or-mpp-pt'].match(regex);
+      return drug['brand-name'].some((name) => name.match(regex)) || drug['tpuu-or-mpp-pt'].match(regex);
     });
 
     if (searchText.length === 0) {
