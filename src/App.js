81b54dfe7c1e365c4c21eaa9e00221f02733f6db
Daniel <daniel.moderiano@outlook.com>
2021-11-17 17:29:13 +1030

    Add Routes for home and template (home to contain Rxform)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyles from "./components/utils/globalStyles";
 import RxForm from './components/RxForm/RxForm'
 import RxTemplate from "./components/RxTemplate/RxTemplate";
 import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router";
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     drugData: {},
     patientData: {},
     providerData: {},
+    miscData: {},
   });
 
   const handleSubmit = (drugData, patientData, providerData, miscData) => {
@@ -29,51 +31,52 @@ const App = () => {
         ...miscData
       },
     }));
+    console.log(data);
   }
 
-  // const dummyData = {
-  //   "drugData": {
-  //       activeIngredient: "latanoprost 0.005% eye drops, 5 mL",
-  //       "brandName":"Xalatan",
-  //       "quantity":"1",
-  //       "repeats":"4",
-  //       "dosage":"Once nightly both eyes",
-  //       "itemCode":"5552F"
-  //   },
-  //     "patientData": {
-  //       "fullName":"Daniel Moderiano",
-  //       "streetAddress":"6 Carragarmungee Estate Long Road Name",
-  //       "subpremise":"Unit 12",
-  //       "suburb":"Port Bonython",
-  //       "postcode":"5127",
-  //       "state":"SA",
-  //       "medicareNumber":"5151515151",
-  //       "medicareRefNumber":"3"
-  //   },
-  //     "providerData": {
-  //       "prefix": true,
-  //       "fullName":"Sarah Smoker",
-  //       "qualifications":"BMedSc(VisSc), MOpt",
-  //       "practiceName":"OPSM",
-  //       "streetAddress":"111 West Lakes Boulevard",
-  //       "subpremise":"Shop 218",
-  //       "suburb":"Modbury",
-  //       "postcode":"5092",
-  //       "state":"SA",
-  //       "phoneNumber":"0882345678",
-  //       "prescriberNumber":"7033149"
-  //   },
-  //     "miscData": {
-  //       substitutePermitted: false,   
-  //       brandOnly: false, 
-  //       includeBrand: true,    
-  //       pbsRx: true,  
-  //       authRxNumber: '',   
-  //       date: '', 
-  //       authRequired: true,
-  //       authCode: '7979',
-  //     }
-  // }
+  const dummyData = {
+    "drugData": {
+        activeIngredient: "latanoprost 0.005% eye drops, 5 mL",
+        "brandName":"Xalatan",
+        "quantity":"1",
+        "repeats":"4",
+        "dosage":"Once nightly both eyes",
+        "itemCode":"5552F"
+    },
+      "patientData": {
+        "fullName":"Daniel Moderiano",
+        "streetAddress":"6 Carragarmungee Estate Long Road Name",
+        "subpremise":"Unit 12",
+        "suburb":"Port Bonython",
+        "postcode":"5127",
+        "state":"SA",
+        "medicareNumber":"5151515151",
+        "medicareRefNumber":"3"
+    },
+      "providerData": {
+        "prefix": true,
+        "fullName":"Sarah Smoker",
+        "qualifications":"BMedSc(VisSc), MOpt",
+        "practiceName":"OPSM",
+        "streetAddress":"111 West Lakes Boulevard",
+        "subpremise":"Shop 218",
+        "suburb":"Modbury",
+        "postcode":"5092",
+        "state":"SA",
+        "phoneNumber":"0882345678",
+        "prescriberNumber":"7033149"
+    },
+      "miscData": {
+        substitutePermitted: false,   
+        brandOnly: false, 
+        includeBrand: true,    
+        pbsRx: true,  
+        authRxNumber: '',   
+        date: '', 
+        authRequired: true,
+        authCode: '7979',
+      }
+  }
 
   return (
     <div className="App">
@@ -82,9 +85,14 @@ const App = () => {
       <Header />
       {/* Note prescriptions must contain date of issue, and prescriber signature */}
       <main>
-        <section className="rx-form">
+        {/* <section className="rx-form">
           <RxForm handleSubmit={handleSubmit}/>
-        </section>
+        </section> */}
+
+        <Switch>
+          <Route exact path="/" render={() => <RxForm handleSubmit={handleSubmit}/>}/>
+          <Route exact path="/template" render={() => <RxTemplate data={dummyData}/>}/>
+        </Switch>
       </main>
       <footer className="footer"></footer>
     </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Nav = () => {
   return (
     <nav className="Nav">
       <ul className="Nav__list">
-          <li className="Nav__list-item">Home</li>
-          <li className="Nav__list-item">About</li>
-        </ul>
+        <li className="Nav__list-item">
+          <Link className="Nav__link" to="/">Home</Link>
+        </li>
+        <li className="Nav__list-item">
+          <Link className="Nav__link" to="/template">Template</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
