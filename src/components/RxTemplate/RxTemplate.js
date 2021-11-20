a596b42b42035ac7077fd4d256bd514c541b08d7
Daniel <daniel.moderiano@outlook.com>
2021-11-20 15:58:52 +1030

    Duplicate Rx template for right and left sides on printed Rx

--- src/components/RxTemplate/RxTemplate.js
+++ src/components/RxTemplate/RxTemplate.js
@@ -78,78 +78,153 @@ const RxTemplate = ({ data, date }) => {
   return (
     <StyledRxTemplate className="RxTemplate">
       <img src={Rx} alt="" />
+      <div className="left-container">
+        <section className="provider-upper">
+          <div className="container">
+            <div className="provider__contact-upper">
+              <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
+              {formatProvAddress()}
+              <div className="provider__addressLine2">
+                {`${providerData.suburb} ${providerData.state} ${providerData.postcode}`}
+              </div>
+            </div>
+            <div className="provider__contact-lower">
+              <div className="provider__prescriberNumber">{providerData.prescriberNumber}</div>
+              <div data-testid="phone" className="provider__phoneNumber">
+                {`Phone: ${formatPhoneNumber(providerData.phoneNumber)}`}
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="patient">
+          <div className="container">
+            <div className="patient__medicareNumber">
+              {`${patientData.medicareNumber.substring(0, 4)} ${patientData.medicareNumber.substring(4, 9)} ${patientData.medicareNumber.substring(9, 10)}-${patientData.medicareRefNumber}`}
+            </div>
+            <div className="patient__contactDetails">
+              <div className="patient__fullName">{patientData.fullName}</div>
+              <div className="patient__streetAddress">{`${patientData.subpremise} ${patientData.streetAddress}`}</div>
+              <div className="patient__addressLine2">{`${patientData.suburb} ${patientData.state} ${patientData.postcode}`}</div>
+            </div>
+          </div>
+        </section>
+
+        <section className="miscellaneous">
+          {/* Include Script ID and Authority Rx number here */}
+          <div className="date">{formatDate()}</div>
+          {drugData.pbsRx 
+            ? <div className="pbsSelected"><img src={tickbox} alt="" /></div>
+            : <div className="nonPbs"><span className="nonPbs-marker">XXXXXXXXXXX</span>Non-PBS</div>
+          }
+          {!drugData.substitutePermitted && <div className="brandSub">✓</div>}
+          
+        </section>
+
+        {/* Script ID or authority Rx number should go above the medication once finalised, and perhaps with a border bottom */}
+        <section className="medication">
+          <div data-testid="drugName" className="medication__activeIngredient">
+            {formatDrug(drugData.activeIngredient, drugData.brandName)}
+          </div>
+          <div className="medication__dosage">{drugData.dosage}</div>
+          <div className="quantityRepeats">
+            <div className="medication__quantity">{`Quantity: ${drugData.quantity}`}</div>
+            <div className="medication__repeats">{`${drugData.repeats} repeats`}</div>
+          </div>
+          <span className="item-printed">1 item printed</span>
+        </section>
+        {/* Will only ever be 1 item printed, so consider omitting this */}
+
+        <section className="provider-lower">
+          {/* Used to display provider details next to, or below signature space */}
+          <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
+          {/* Qualifications should only be included in the lower section */}
+          <div className="provider__qualifications">{providerData.qualifications}</div>
+          <div className="practitionerTick">🗸</div>
+        </section>
+
+        {/* Wastes space to render authority section for non-authority required scripts, so render only as needed */}
+        {drugData.authRequired && <section className="authority">
+          <div className="authority__approvalCode">{`Authority Approval No: ${miscData.authCode}`}</div>
+          {/* Optional sections below - not sure how useful these are in this day and age */}
+          {/* <div className="authority__authorised">Authorised:</div>
+          <div className="authority__delegate">Delegate...............</div> */}
+        </section>}
+      </div>
+      <div className="right-container">
+        <section className="provider-upper">
+          <div className="container">
+            <div className="provider__contact-upper">
+              <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
+              {formatProvAddress()}
+              <div className="provider__addressLine2">
+                {`${providerData.suburb} ${providerData.state} ${providerData.postcode}`}
+              </div>
+            </div>
+            <div className="provider__contact-lower">
+              <div className="provider__prescriberNumber">{providerData.prescriberNumber}</div>
+              <div data-testid="phone" className="provider__phoneNumber">
+                {`Phone: ${formatPhoneNumber(providerData.phoneNumber)}`}
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="patient">
+          <div className="container">
+            <div className="patient__medicareNumber">
+              {`${patientData.medicareNumber.substring(0, 4)} ${patientData.medicareNumber.substring(4, 9)} ${patientData.medicareNumber.substring(9, 10)}-${patientData.medicareRefNumber}`}
+            </div>
+            <div className="patient__contactDetails">
+              <div className="patient__fullName">{patientData.fullName}</div>
+              <div className="patient__streetAddress">{`${patientData.subpremise} ${patientData.streetAddress}`}</div>
+              <div className="patient__addressLine2">{`${patientData.suburb} ${patientData.state} ${patientData.postcode}`}</div>
+            </div>
+          </div>
+        </section>
+
+        <section className="miscellaneous">
+          {/* Include Script ID and Authority Rx number here */}
+          <div className="date">{formatDate()}</div>
+          {drugData.pbsRx 
+            ? <div className="pbsSelected"><img src={tickbox} alt="" /></div>
+            : <div className="nonPbs"><span className="nonPbs-marker">XXXXXXXXXXX</span>Non-PBS</div>
+          }
+          {!drugData.substitutePermitted && <div className="brandSub">✓</div>}
+          
+        </section>
+
+        {/* Script ID or authority Rx number should go above the medication once finalised, and perhaps with a border bottom */}
+        <section className="medication">
+          <div data-testid="drugName" className="medication__activeIngredient">
+            {formatDrug(drugData.activeIngredient, drugData.brandName)}
+          </div>
+          <div className="medication__dosage">{drugData.dosage}</div>
+          <div className="quantityRepeats">
+            <div className="medication__quantity">{`Quantity: ${drugData.quantity}`}</div>
+            <div className="medication__repeats">{`${drugData.repeats} repeats`}</div>
+          </div>
+          <span className="item-printed">1 item printed</span>
+        </section>
+        {/* Will only ever be 1 item printed, so consider omitting this */}
+
+        <section className="provider-lower">
+          {/* Used to display provider details next to, or below signature space */}
+          <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
+          {/* Qualifications should only be included in the lower section */}
+          <div className="provider__qualifications">{providerData.qualifications}</div>
+          <div className="practitionerTick">🗸</div>
+        </section>
+
+        {/* Wastes space to render authority section for non-authority required scripts, so render only as needed */}
+        {drugData.authRequired && <section className="authority">
+          <div className="authority__approvalCode">{`Authority Approval No: ${miscData.authCode}`}</div>
+          {/* Optional sections below - not sure how useful these are in this day and age */}
+          {/* <div className="authority__authorised">Authorised:</div>
+          <div className="authority__delegate">Delegate...............</div> */}
+        </section>}
+      </div>
       
-      <section className="provider-upper">
-        <div className="container">
-          <div className="provider__contact-upper">
-            <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
-            {formatProvAddress()}
-            <div className="provider__addressLine2">
-              {`${providerData.suburb} ${providerData.state} ${providerData.postcode}`}
-            </div>
-          </div>
-          <div className="provider__contact-lower">
-            <div className="provider__prescriberNumber">{providerData.prescriberNumber}</div>
-            <div data-testid="phone" className="provider__phoneNumber">
-              {`Phone: ${formatPhoneNumber(providerData.phoneNumber)}`}
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="patient">
-        <div className="container">
-          <div className="patient__medicareNumber">
-            {`${patientData.medicareNumber.substring(0, 4)} ${patientData.medicareNumber.substring(4, 9)} ${patientData.medicareNumber.substring(9, 10)}-${patientData.medicareRefNumber}`}
-          </div>
-          <div className="patient__contactDetails">
-            <div className="patient__fullName">{patientData.fullName}</div>
-            <div className="patient__streetAddress">{`${patientData.subpremise} ${patientData.streetAddress}`}</div>
-            <div className="patient__addressLine2">{`${patientData.suburb} ${patientData.state} ${patientData.postcode}`}</div>
-          </div>
-        </div>
-      </section>
-
-      <section className="miscellaneous">
-        {/* Include Script ID and Authority Rx number here */}
-        <div className="date">{formatDate()}</div>
-        {drugData.pbsRx 
-          ? <div className="pbsSelected"><img src={tickbox} alt="" /></div>
-          : <div className="nonPbs"><span className="nonPbs-marker">XXXXXXXXXXX</span>Non-PBS</div>
-        }
-        {!drugData.substitutePermitted && <div className="brandSub">✓</div>}
-        
-      </section>
-
-      {/* Script ID or authority Rx number should go above the medication once finalised, and perhaps with a border bottom */}
-      <section className="medication">
-        <div data-testid="drugName" className="medication__activeIngredient">
-          {formatDrug(drugData.activeIngredient, drugData.brandName)}
-        </div>
-        <div className="medication__dosage">{drugData.dosage}</div>
-        <div className="quantityRepeats">
-          <div className="medication__quantity">{`Quantity: ${drugData.quantity}`}</div>
-          <div className="medication__repeats">{`${drugData.repeats} repeats`}</div>
-        </div>
-        <span className="item-printed">1 item printed</span>
-      </section>
-      {/* Will only ever be 1 item printed, so consider omitting this */}
-
-      <section className="provider-lower">
-        {/* Used to display provider details next to, or below signature space */}
-        <div className="provider__fullName">{`${providerData.prefix ? 'Dr' : ''} ${providerData.fullName}`}</div>
-        {/* Qualifications should only be included in the lower section */}
-        <div className="provider__qualifications">{providerData.qualifications}</div>
-        <div className="practitionerTick">🗸</div>
-      </section>
-
-      {/* Wastes space to render authority section for non-authority required scripts, so render only as needed */}
-      {drugData.authRequired && <section className="authority">
-        <div className="authority__approvalCode">{`Authority Approval No: ${miscData.authCode}`}</div>
-        {/* Optional sections below - not sure how useful these are in this day and age */}
-        {/* <div className="authority__authorised">Authorised:</div>
-        <div className="authority__delegate">Delegate...............</div> */}
-      </section>}
       
     </StyledRxTemplate>
   );
--- src/components/RxTemplate/RxTemplate.styled.js
+++ src/components/RxTemplate/RxTemplate.styled.js
@@ -6,6 +6,7 @@ const StyledRxTemplate = styled.div`
   position: relative;
   margin: 0;
   padding: 0;
+  display: flex;
 
   img {
     position: absolute;
@@ -23,164 +24,336 @@ const StyledRxTemplate = styled.div`
   width: 902px;
   height: 1275px;
 
-  .container {
-    position: relative;
-    width: 100%;
-    height: 100%;
-  }
-
-  .provider-upper {
-    position: absolute;
-    width: 100%;
-  }
-
-  .provider__contact-upper {
-    position: absolute;
-    left: 90px;
-    top: 20px;
-
-    .provider__streetAddress {
-      max-width: 330px;
+  .right-container {
+    
+    .container {
+      position: relative;
+      width: 100%;
+      height: 100%;
     }
-  }
 
-  .provider__contact-lower {
-    position: absolute;
-    left: 90px;
-    top: 103px;
-    display: flex;
-    justify-content: space-between;
-    width: 300px;
-  }
-
-  .patient {
-    position: absolute;
-  }
-
-  .patient__contactDetails {
-    position: absolute;
-    left: 100px;
-    top: 240px;
-    width: 300px;
-    font-weight: bold;
-  }
-
-  .patient__medicareNumber {
-    position: absolute;
-    display: flex;
-    justify-content: space-between;
-    top: 138px;
-    left: 140px;
-    width: 110px;
-    font-weight: bold;
-  }
-
-  .miscellaneous {
-    position: absolute;
-    top: 311px;
-    left: 40px;
-
-    /* Consider date reduced font size */
-    /* .date {
-      font-size: 14px;
-    } */
-
-    .pbsSelected {
-      width: 17px;
-      height: 17px;
-      display: block;
-      /* Without position the img will size relative to the miscellaneous div above */
+    .provider-upper {
       position: absolute;
+      width: 450px;
+    }
+
+    .provider__contact-upper {
+      position: absolute;
+      left: 90px;
       top: 20px;
 
-      img {
-        height: 100%;
-        width: 100%;
+      .provider__streetAddress {
+        max-width: 330px;
       }
     }
 
-    .nonPbs {
+    .provider__contact-lower {
       position: absolute;
-      top: 27px;
-      left: -18px;
-      padding-top: 12px;
-      width: 80px;
-      box-sizing: border-box;
-
-      .nonPbs-marker {
-        bottom: 17px;
-        left: -16px;
-        position: absolute;
-        width: 150px;
-        font-size: 17px;
-      }
-    }
-    
-    .brandSub {
-      position: absolute;
-      left: 148px;
-      top: 12px;
-      font-size: 24px;
-    }
-  }
-
-  .medication {
-    position: absolute;
-    left: 112px;
-    top: 366px;
-    width: 330px;
-    border-bottom: 1px solid #b8b8b8;
-    padding: 2px 0;
-
-    .medication__activeIngredient {
-      font-weight: bold;
-      max-width: 330px;
-    }
-
-    .medication__brandName {
-      font-weight: bold;
-    }
-
-    .quantityRepeats {
+      left: 90px;
+      top: 103px;
       display: flex;
       justify-content: space-between;
-      width: 170px;
+      width: 300px;
     }
 
-    .item-printed {
+    .patient {
       position: absolute;
-      bottom: -22px;
-      right: 2px;
     }
-  }
 
-  .provider-lower {
-    position: absolute;
-    left: 112px;
-    top: 502px;
-    width: 330px;
-    
-    .practitionerTick {
+    .patient__contactDetails {
       position: absolute;
-      left: 315px;
-      top: 134px;
-      font-size: 26px;
+      left: 100px;
+      top: 240px;
+      width: 300px;
+      font-weight: bold;
+    }
+
+    .patient__medicareNumber {
+      position: absolute;
+      display: flex;
+      justify-content: space-between;
+      top: 138px;
+      left: 140px;
+      width: 110px;
+      font-weight: bold;
+    }
+
+    .miscellaneous {
+      position: absolute;
+      top: 311px;
+      left: 40px;
+
+      /* Consider date reduced font size */
+      /* .date {
+        font-size: 14px;
+      } */
+
+      .pbsSelected {
+        width: 17px;
+        height: 17px;
+        display: block;
+        /* Without position the img will size relative to the miscellaneous div above */
+        position: absolute;
+        top: 20px;
+
+        img {
+          height: 100%;
+          width: 100%;
+        }
+      }
+
+      .nonPbs {
+        position: absolute;
+        top: 27px;
+        left: -18px;
+        padding-top: 12px;
+        width: 80px;
+        box-sizing: border-box;
+
+        .nonPbs-marker {
+          bottom: 17px;
+          left: -16px;
+          position: absolute;
+          width: 150px;
+          font-size: 17px;
+        }
+      }
+      
+      .brandSub {
+        position: absolute;
+        left: 148px;
+        top: 12px;
+        font-size: 24px;
+      }
+    }
+
+    .medication {
+      position: absolute;
+      left: 112px;
+      top: 366px;
+      width: 330px;
+      border-bottom: 1px solid #b8b8b8;
+      padding: 2px 0;
+
+      .medication__activeIngredient {
+        font-weight: bold;
+        max-width: 330px;
+      }
+
+      .medication__brandName {
+        font-weight: bold;
+      }
+
+      .quantityRepeats {
+        display: flex;
+        justify-content: space-between;
+        width: 170px;
+      }
+
+      .item-printed {
+        position: absolute;
+        bottom: -22px;
+        right: 2px;
+      }
+    }
+
+    .provider-lower {
+      position: absolute;
+      left: 112px;
+      top: 502px;
+      width: 330px;
+      
+      .practitionerTick {
+        position: absolute;
+        left: 315px;
+        top: 134px;
+        font-size: 26px;
+        font-weight: bold;
+      }
+    }
+
+    .authority {
+      border-top: 1px solid #b8b8b8;
+      padding-top: 2px;
+      position: absolute;
+      left: 112px;
+      top: 540px;
+      width: 330px;
+    }
+
+    .provider__fullName {
       font-weight: bold;
     }
   }
 
-  .authority {
-    border-top: 1px solid #b8b8b8;
-    padding-top: 2px;
+/* ##### LEFT CONTAINER ##### */
+
+  .left-container {
     position: absolute;
-    left: 112px;
-    top: 540px;
-    width: 330px;
+    left: 456px;
+    /* width: 400px; */
+
+    .container {
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+
+    .provider-upper {
+      position: absolute;
+      width: 450px;
+    }
+
+    .provider__contact-upper {
+      position: absolute;
+      left: 90px;
+      top: 20px;
+
+      .provider__streetAddress {
+        max-width: 330px;
+      }
+    }
+
+    .provider__contact-lower {
+      position: absolute;
+      left: 90px;
+      top: 103px;
+      display: flex;
+      justify-content: space-between;
+      width: 300px;
+    }
+
+    .patient {
+      position: absolute;
+    }
+
+    .patient__contactDetails {
+      position: absolute;
+      left: 100px;
+      top: 240px;
+      width: 300px;
+      font-weight: bold;
+    }
+
+    .patient__medicareNumber {
+      position: absolute;
+      display: flex;
+      justify-content: space-between;
+      top: 138px;
+      left: 140px;
+      width: 110px;
+      font-weight: bold;
+    }
+
+    .miscellaneous {
+      position: absolute;
+      top: 311px;
+      left: 40px;
+
+      /* Consider date reduced font size */
+      /* .date {
+        font-size: 14px;
+      } */
+
+      .pbsSelected {
+        width: 17px;
+        height: 17px;
+        display: block;
+        /* Without position the img will size relative to the miscellaneous div above */
+        position: absolute;
+        top: 20px;
+
+        img {
+          height: 100%;
+          width: 100%;
+        }
+      }
+
+      .nonPbs {
+        position: absolute;
+        top: 27px;
+        left: -18px;
+        padding-top: 12px;
+        width: 80px;
+        box-sizing: border-box;
+
+        .nonPbs-marker {
+          bottom: 17px;
+          left: -16px;
+          position: absolute;
+          width: 150px;
+          font-size: 17px;
+        }
+      }
+      
+      .brandSub {
+        position: absolute;
+        left: 148px;
+        top: 12px;
+        font-size: 24px;
+      }
+    }
+
+    .medication {
+      position: absolute;
+      left: 112px;
+      top: 366px;
+      width: 330px;
+      border-bottom: 1px solid #b8b8b8;
+      padding: 2px 0;
+
+      .medication__activeIngredient {
+        font-weight: bold;
+        max-width: 330px;
+      }
+
+      .medication__brandName {
+        font-weight: bold;
+      }
+
+      .quantityRepeats {
+        display: flex;
+        justify-content: space-between;
+        width: 170px;
+      }
+
+      .item-printed {
+        position: absolute;
+        bottom: -22px;
+        right: 2px;
+      }
+    }
+
+    .provider-lower {
+      position: absolute;
+      left: 112px;
+      top: 502px;
+      width: 330px;
+      
+      .practitionerTick {
+        position: absolute;
+        left: 315px;
+        top: 134px;
+        font-size: 26px;
+        font-weight: bold;
+      }
+    }
+
+    .authority {
+      border-top: 1px solid #b8b8b8;
+      padding-top: 2px;
+      position: absolute;
+      left: 112px;
+      top: 540px;
+      width: 330px;
+    }
+
+    .provider__fullName {
+      font-weight: bold;
+    }
   }
 
-  .provider__fullName {
-    font-weight: bold;
-  }
+  
 
   @media print {
     @page {
