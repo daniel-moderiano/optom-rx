705fd597636258751a34e055c73ef50b0d9df0ed
Daniel <daniel.moderiano@outlook.com>
2021-11-30 17:41:53 +1030

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Web app config file
+const firebaseConfig = {
+  apiKey: "AIzaSyBvzkDigW4wgRvq0vEbs3M8COZD9ZrbY3Q",
+  authDomain: "optom-rx-dcb2e.firebaseapp.com",
+  projectId: "optom-rx-dcb2e",
+  storageBucket: "optom-rx-dcb2e.appspot.com",
+  messagingSenderId: "1057936692796",
+  appId: "1:1057936692796:web:851c581c23ee6300db1539"
+};
+
+// Initialise Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialise Firestore database
+const db = getFirestore();
+
+// Initialise Firebase authentication
+const auth = getAuth();
+
+export { db, auth }
