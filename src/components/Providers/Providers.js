0495017b846a2134624af7b3ca11fd0991c889cc
Daniel <daniel.moderiano@outlook.com>
2021-11-30 22:05:54 +1030

    Merge auth

---
+++ src/components/Providers/Providers.js
@@ -0,0 +1,10 @@
+const Providers = () => {
+  return (
+    <div className="Providers">
+      <h2 className="Providers__title">Providers</h2>
+      <button className="Providers__add-btn">Add new provider</button>
+    </div>
+  )
+}
+
+export default Providers
