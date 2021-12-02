71913968eda9f7e6f8c70ce40f00c82113d51f33
Daniel <daniel.moderiano@outlook.com>
2021-12-02 10:39:21 +1030

    Show only the logged in user's providers

--- src/components/Providers/Providers.js
+++ src/components/Providers/Providers.js
@@ -1,29 +1,39 @@
 import ProviderForm from "../ProviderForm/ProviderForm";
 import { useCollection } from '../../hooks/useCollection';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useState } from "react";
 
 const Providers = () => {
   const { user } = useAuthContext();
   // This should be called using the curernt user ID to query the collection
   // Query should be ('providers', ['uid', '==', user.uid])
-  const { documents: providers } = useCollection('providers');
+  const { documents: providers } = useCollection('providers', ['uid', '==', user.uid]);
   
+  const [showForm, setShowForm] = useState(false);
+
+  const showProviderForm = () => {
+    if (!showForm) {
+      setShowForm((prevState) => !prevState);
+    }
+
+  };
 
   return (
     <div className="Providers">
       <h2 className="Providers__title">Providers</h2>
       <p className="Providers__description">Use this section to add provider details that can be used in your prescriptions</p>
-      <button className="Providers__add-btn">Add new provider</button>
+      <button className="Providers__add-btn" onClick={showProviderForm}>Add new provider</button>
       {providers && 
         <div className="Providers__list">
         {/* Render providers using map function here */}
         {providers.map(provider => (
-          <div className="Providers__list-item">{provider.fullName}</div>
+          <div key={provider.id} className="Providers__list-item">{provider.fullName}</div>
         ))}
         </div>
       }
       
-      <ProviderForm standalone={true}/>
+      {showForm && <ProviderForm standalone={true}/>}
+      
     </div>
   )
 }
