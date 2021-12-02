256ea4b2a6cf58a75c03535f69f375b72f698284
Daniel <daniel.moderiano@outlook.com>
2021-12-02 22:34:07 +1030

    Add outside click handling for user menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import { StyledNav } from "./Nav.styled";
 import { useLogout } from '../../hooks/useLogout';
 import UserMenu from "../UserMenu/UserMenu";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 const Nav = () => {
   // Bring in the logout function from the custom hook so that it can be called on click via Logout btn
   const { logout } = useLogout();
 
-  const [showMenu, setShowMenu] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
 
   // Ensures that navigating to template route via Nav does not try to display a template with no data
   const templateLocation = {
@@ -20,6 +20,23 @@ const Nav = () => {
   const toggleMenu = () => {
     setShowMenu((prevState) => !prevState);
   };
+
+  // Ensure the items list closes on outside click
+  const menuOutsideClick = useCallback((event) => {
+    // All items within the autocomplete input and items list will contain UserMenu in their class. Note also the dropdown btn has the same class to ensure no clashing of toggle functions
+    if (!event.target.className.includes('UserMenu')) {
+      setShowMenu(false);
+    }      
+  }, []);
+
+  // Runs once only on initial mount, and cleans up on dismount
+  useEffect(() => {
+    window.addEventListener('click', menuOutsideClick);
+
+    return () => {
+      window.removeEventListener('click', menuOutsideClick)
+    }
+  }, [menuOutsideClick])
 
   return (
     <StyledNav className="Nav">
@@ -43,7 +60,7 @@ const Nav = () => {
           <Link className="Nav__link" to={templateLocation}>Template</Link>
         </li>
         <li className="Nav__list-item">
-          <button className="Nav__link" onClick={toggleMenu}>Dropdown</button>
+          <button className="Nav__link UserMenu__toggle" onClick={toggleMenu}>Dropdown</button>
         </li>
       </ul>
       {showMenu && <UserMenu handleClick={toggleMenu}/>}
