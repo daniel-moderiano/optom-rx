5e328ec7dccee266d1a720ef3bfe0fc00b45f942
Daniel <daniel.moderiano@outlook.com>
2021-12-04 17:02:43 +1030

    Add update feature to useNumbers so there are no duplicate auth or script numbers being fetched

--- src/hooks/useNumbers.js
+++ src/hooks/useNumbers.js
@@ -2,7 +2,7 @@ import { db } from "../firebase/config";
 import { useState, useEffect } from "react";
 
 // Firebase imports
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 // A hook that fetches the current script number from the backend, using a single getDoc call (as opposed to snapshot real time updates), and subsequently increments this number and updates the backend ready for subsequent calls
 export const useNumbers = () => {
@@ -12,10 +12,46 @@ export const useNumbers = () => {
   const [isError, setIsError] = useState(false);
   console.log('call useNumbers');
 
+  // Correctly increment the base auth Rx number to be updated on the backend
+  const incrementAuthRxNumber = (prevNumber) => {
+    // Ensure the counter is 'reset' to 0000000 if the previous number is at the theoretical limit of 9999999
+    if (prevNumber === '9999999') {
+      return '0000000';
+    }
+    
+    // First convert to base 10
+    const base10 = parseInt(prevNumber, 10);
+    const incremented = base10 + 1;
+    let newNum = incremented.toString();
+
+    // Ensure the length is at required seven by adding leading zeroes as needed
+    while (newNum.length < 7) {
+      newNum = '0' + newNum;
+    }
+
+    return newNum;
+  }
+
+  // Correctly increment the base auth Rx number to be updated on the backend
+  const incrementScriptNumber = (prevNumber) => {
+    // First convert to base 10
+    const base10 = parseInt(prevNumber, 10);
+    const incremented = base10 + 1;
+    let newNum = incremented.toString();
+
+    // Ensure the length meets the arbitrarily chosen 8 digits by adding leading zeroes until no longer applicable
+    // This ensures around 800 years' worth of prescriptions before incrementing to a 9 digit number, for interest :)
+    while (newNum.length < 8) {
+      newNum = '0' + newNum;
+    }
+
+    return newNum;
+  }
+
   useEffect(() => {
     // Note there will only ever be one 'current' field in each document in this collection. There are only two documents: scriptNo and authRxNo. They should never need to be called separately.
-    const scriptNo = doc(db, 'numbers', 'scriptNo');
-    const authRxNo = doc(db, 'numbers', 'authRxNo');
+    const scriptNoRef = doc(db, 'numbers', 'scriptNo');
+    const authRxNoRef = doc(db, 'numbers', 'authRxNo');
 
     // Initialise error and loading state
     setIsLoading(true);
@@ -39,9 +75,9 @@ export const useNumbers = () => {
     // };
 
     const fetchData = () => {
-      // Define promises for each doc
-      const scriptNoSnap = getDoc(scriptNo);
-      const authRxNoSnap = getDoc(authRxNo); 
+      // Define promises for fetching each number
+      const scriptNoSnap = getDoc(scriptNoRef);
+      const authRxNoSnap = getDoc(authRxNoRef); 
 
       // Handle the collective responses
       Promise.all([scriptNoSnap, authRxNoSnap])
@@ -49,14 +85,22 @@ export const useNumbers = () => {
           // Handle the data
           setScriptNo(script.data().current);
           setAuthRxNo(auth.data().current);
-          setIsLoading(false);
-          console.log('done');
+          return [script.data().current, auth.data().current];
+        })
+        .then(([scriptNoCurrent, authRxNoCurrent]) => {
+          // This block will only execute once the above operations are complete
+          updateDoc(scriptNoRef, {
+            current: incrementScriptNumber(scriptNoCurrent)
+          });
+          updateDoc(authRxNoRef, {
+            current: incrementAuthRxNumber(authRxNoCurrent)
+          }); 
         })
         .catch((error) => {
-          setIsError(true);
-          setIsLoading(false);
+          setIsError(true); 
         }) 
-      
+        // In either case (resolve or reject), the loading is done
+        .finally(() => setIsLoading(false));
     };
 
     fetchData();
