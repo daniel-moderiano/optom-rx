3ee01ef20ff7e36e4e4511e4d5b217f012f9f8ec
Daniel <daniel.moderiano@outlook.com>
2021-12-05 16:28:01 +1030

    Fix Home -> NewRx Link with v6 syntax

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,16 @@
 import { StyledHome } from './Home.styled';
 import { Link } from 'react-router-dom';
-import { useNumbers } from '../../hooks/useNumbers';
-import { useState } from 'react';
 
 const Home = () => {
-  const location = {
-    pathname: '/form',
-    state: { newRx: true }
-  }
-
-  const [{ scriptNo, authRxNo, isError, isLoading }, fetchNumbers] = useNumbers();
-  const [numState, setNumState] = useState({ script: '', auth: '' })
 
   return (
     <StyledHome className="Home">
       <h2 className="Home__title">Home page</h2>
       <div className="Home__welcome">Welcome to OptomRx</div>
       <div className="Home__links">
-        <Link className="Home__link" to={location}>Create new prescription</Link>
+        <Link className="Home__link" to='/form' state={ { newRx: true } }>Create new prescription</Link>
         <Link className="Home__link" to="/login">Login</Link>
       </div>
-
-      <button 
-        onClick={() => {
-          fetchNumbers().then(() => {setNumState((prevState) => ({ ...prevState, script: scriptNo, auth: authRxNo }))})}}>Fetch
-      </button>
 
     </StyledHome>
   )
