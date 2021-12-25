07f9c970c7edbce5bfae5d8f1aead490432005ae
Daniel <daniel.moderiano@outlook.com>
2021-12-25 22:26:49 +1030

    Style login form, and add links for sign up and cancel

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,29 @@
 import AuthForm from "../AuthForm/AuthForm";
 import { useLogin } from '../../hooks/useLogin';
+import { Link } from "react-router-dom";
+import { StyledLogin } from "./Login.styled";
 
 const Login = () => {
   const { error, login } = useLogin();
 
   return (
-    <div className="Login">
-      <h2 className="Login__title">Login</h2>
+    <StyledLogin className="Login">
+      <h2 className="Login__title">Log in</h2>
       <AuthForm 
         submitFunc={login}
-        buttonLabel="Login"
+        buttonLabel="Log in"
       />
+      <div className="signup-option">
+        <Link to="/" className="home-link">Cancel</Link>
+        <div className="signup-group">
+          <span className="signup-msg">Don't have an account?</span>
+          <Link to="/signup" className="signup-link">Sign Up.</Link>
+        </div>
+        
+      </div>
+      
       {error && <p>{error}</p>}
-    </div>
+    </StyledLogin>
   )
 }
 
---
+++ src/components/Login/Login.styled.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+const StyledLogin = styled.div`
+  margin-top: 3rem;
+  background-color: #FFFFFF;
+  box-shadow: 0 1.6px 3.6px 0 rgb(0 0 0 / 13%), 0 0.3px 0.9px 0 rgb(0 0 0 / 11%);
+  padding: 1rem 3rem;
+  width: 90%;
+  max-width: 30rem;
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    button {
+      width: 24rem;
+      font-size: 1rem;
+      font-family: var(--font-stack-segoe);
+      box-sizing: border-box;
+      cursor: pointer;
+      padding: 8px 16px 10px 16px;
+      border-radius: 2px;
+      min-width: 80px;
+      background-color: var(--primary-color);
+      color: rgb(255, 255, 255);
+      border: none;
+      margin: 2rem 0 1rem 0;
+
+      &:active {
+        transform: scale(0.98);
+      }
+
+      &:hover {
+        background-color: var(--btn-primary-hover);
+      }
+    }
+  }
+
+  .Login__title {
+    place-self: flex-start;
+    font-family: var(--font-stack-myriad);
+    font-weight: 400;
+    color: var(--title-color);
+    font-size: 2.5rem;
+    margin: 1rem 0 0.75rem 0;
+  }
+
+  .signup-option {
+    width: 24rem;
+    /* padding-left: 3rem; */
+    margin-bottom: 1.5rem;
+    display: flex;
+    justify-content: space-between;
+
+    .signup-msg {
+      color: #5A6572;
+      margin-right: 0.4rem;
+    }
+
+    .signup-link, .home-link {
+      text-decoration: none;
+      color: var(--primary-color);
+      font-weight: bold;
+      font-size: 1rem;
+    }
+  }
+
+  
+
+`
+
+export { StyledLogin }
