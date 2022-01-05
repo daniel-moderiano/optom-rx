e0656bd7f88e3b86ec30378e10633163789cef2b
Daniel <daniel.moderiano@outlook.com>
2022-01-05 14:54:26 +1030

    switch save/finish button to programatically navigate home after save complete

--- src/components/RxTemplate/RxTemplate.js
+++ src/components/RxTemplate/RxTemplate.js
@@ -2,7 +2,7 @@ import { StyledRxTemplate } from "./RxTemplate.styled";
 import tickbox from '../../assets/tickbox.svg';
 import lightTick from '../../assets/light-tick.svg';
 import largeTick from '../../assets/large-tick.svg';
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import { db } from '../../firebase/config';
 import { arrayUnion, doc, setDoc, updateDoc } from "firebase/firestore";
@@ -15,6 +15,7 @@ const RxTemplate = ({ data, setToast }) => {
 
   // State is null by default, so checking for it's existence alone will ensure this is a valid link containing data vs a simple nav link
   let { state } = useLocation();
+  let navigate = useNavigate();
 
   const { user } = useAuthContext();
 
@@ -109,6 +110,8 @@ const RxTemplate = ({ data, setToast }) => {
       type: 'success',
       message: 'Prescription saved'
     }));
+
+    navigate('/');
   };
 
   return (
@@ -431,10 +434,10 @@ const RxTemplate = ({ data, setToast }) => {
             </svg>
             Print
           </button>
-          <Link className="RxTemplate__btn btn-finish" to="/" onClick={saveRx}>
+          <button type="button" className="RxTemplate__btn btn-finish" onClick={saveRx}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>Save</title><path d="M380.93 57.37A32 32 0 00358.3 48H94.22A46.21 46.21 0 0048 94.22v323.56A46.21 46.21 0 0094.22 464h323.56A46.36 46.36 0 00464 417.78V153.7a32 32 0 00-9.37-22.63zM256 416a64 64 0 1164-64 63.92 63.92 0 01-64 64zm48-224H112a16 16 0 01-16-16v-64a16 16 0 0116-16h192a16 16 0 0116 16v64a16 16 0 01-16 16z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
             Save and Finish
-          </Link>
+          </button>
         </div>
       </> : <h3 className="RxTemplate__subtitle">Fill out the form to generate Rx</h3>}
       
