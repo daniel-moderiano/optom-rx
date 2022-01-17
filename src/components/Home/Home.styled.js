46e891b2667cefd4e3e013071be142ced664929a
Daniel <daniel.moderiano@outlook.com>
2022-01-17 17:42:53 +1030

    Adjust email modal styling and fix app styling bug with duplicate class names

--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -78,6 +78,104 @@ const StyledHome = styled.div`
     width: 100%;
   }
 
+
+  .modal-btns {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .ok-btn {
+    box-sizing: border-box;
+    font-size: 1rem;
+    font-family: var(--font-stack-segoe);
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 5px 16px 6px 16px;
+    border-radius: 2px;
+    text-decoration: none;
+    /* min-width: 80px; */
+    background-color: #fff;
+    color: #0b4740;
+    border: 1px solid #0b4740;
+    margin-bottom: 1rem;
+
+    &:hover {
+      background-color: var(--btn-positive-hover);
+      color: var(--btn-positive-hover-text);
+    }
+    
+
+    &:active {
+      transform: scale(0.98);
+    }
+
+    &:focus {
+      outline: 2px solid #104362;
+      outline-offset: 2px;
+    }
+  }
+
+  .resend {
+    width: 100%;
+    text-align: center;
+    background-color: transparent;
+    border: none;
+    color: #48515B;
+    /* color: var(--focus); */
+    font-size: 0.9rem;
+    font-family: var(--font-stack-segoe);
+    /* font-weight: bold; */
+    padding: 0;
+    margin: 0;
+
+    &:hover {
+      background-color: transparent;
+      color: #1B1E22;
+      cursor: pointer;
+    }
+
+    &:focus {
+      outline: 2px solid #104362;
+      outline-offset: 1px;
+    }
+
+    &:focus:not(:focus-visible) {
+      outline: none
+    }
+
+    &:focus-visible {
+      outline: 2px solid #104362;
+      outline-offset: 1px;
+    }
+  }
+
+  .img-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem 0;
+
+    .email-svg {
+      width: 75px;
+      pointer-events: none;
+      enable-background:new 0 0 64 64;
+
+      .st0{fill: #77B3D4;}
+
+      .st1{opacity:0.2;}
+
+      .st2{fill:#231F20;}
+
+      .st3{fill:#E0E0D1;}
+
+      .st4{fill:#FFFFFF;}
+  
+    }
+  }
+
   /* Landscape phone to portrait tablet */
   @media (max-width: 768px) { 
     padding: 3rem 3rem;
