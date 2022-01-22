95f162aec504079378f82d6f90cbd70899f28edd
Daniel <daniel.moderiano@outlook.com>
2022-01-22 18:11:16 +1030

    Delete useScriptsRealtime and cleanup useScripts hook

--- src/hooks/useScripts.js
+++ src/hooks/useScripts.js
@@ -4,14 +4,11 @@ import { useState, useEffect } from "react";
 // Firebase imports
 import { doc, onSnapshot } from "firebase/firestore";
 
-// A hook that retrieves all documents in real time from a specifid collection. Can be upgraded to support queries at a later date
+// A hook that retrieves all documents in real time from a specifid collection. Can be upgraded to support queries if needed
 export const useScripts = (userID) => {
   const [scripts, setScripts] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
-
-  // Avoid an infinite loops via useRef (since query is an array)
-  // const docQueryCurrent = useRef(docQuery).current
 
   useEffect(() => {
     // This technically marks the beginning of the fetch call, so set pending state here
@@ -20,28 +17,20 @@ export const useScripts = (userID) => {
     // Get reference to the intended user's user doc (which contains their scripts)
     let ref = doc(db, 'users', userID);
 
-    // Query is optional in this hook
-    // if (docQueryCurrent) {
-    //   ref = query(ref, where(...docQueryCurrent));
-    // }
-
     // Written as an unsub function to unsubscribe once component dismounts
     const unsub = onSnapshot(ref, 
       (snapshot) => {
         setIsPending(false);
         setError(null);
         if (snapshot.data()) {
-         
           // If the data is retrieved this point will be reached, even if there are no scripts
 
           // Ensure newest scripts are displayed at the top of the list
           const scriptData = (snapshot.data().scripts).reverse();
-          
           setScripts(scriptData);
         } else {
           // The document retrieved will be null in this case, and data() undefined, so set an error
-    
-          setError('Failed to fetch scripts')
+          setError('Failed to fetch scripts');
         }
       }, 
       (error) => {
@@ -54,6 +43,5 @@ export const useScripts = (userID) => {
 
   }, [userID])
 
-  // Reference the documents using destructuring in any component
   return { scripts, isPending, error }
 }
