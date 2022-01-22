2d3dfb1fdf7a4fcb90aca1c918e5f9b85d0932e9
Daniel <daniel.moderiano@outlook.com>
2022-01-22 13:23:11 +1030

    Add PrescriberForm to git

---
+++ src/components/PrescriberForm/PrescriberForm.styled.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const StyledPrescriberForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  .fields {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .checkbox {
+    margin: 0.75rem 0 1.25rem 0;
+  }
+
+  .PrescriberForm__btns {
+    width: 100%;
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
+    display: flex;
+
+    .submit {
+      min-width: 130px;
+      margin-right: 1.5rem;
+    }
+  }
+
+  
+  @media (max-width: 590px) { 
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media(max-width: 450px) {
+    .PrescriberForm__btns {
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      .button {
+        width: 90%;
+        margin: 0;
+      }
+
+      .submit {
+        margin-bottom: 1.5rem;
+        min-width: none;
+      }
+    }
+  }
+`
+
+export { StyledPrescriberForm }
---
+++ src/components/PrescriberForm/ProviderForm.test.js
@@ -0,0 +1,71 @@
+import ProviderForm from './ProviderForm';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { AuthContextProvider } from '../../context/AuthContext';
+
+// Used because of the 'google is not defined' error 
+beforeEach(() => {
+  jest.spyOn(console, 'error')
+  // @ts-ignore jest.spyOn adds this functionallity
+  console.error.mockImplementation(() => null);
+
+  render(
+    <AuthContextProvider>
+      <ProviderForm standalone={true} />
+    </AuthContextProvider>  
+  );
+});
+
+afterEach(() => {
+  // @ts-ignore jest.spyOn adds this functionallity
+  console.error.mockRestore()
+})
+
+describe('Provider data input tests', () => {
+  test('Provider data input initialises with blank values', () => {
+    const presNo = screen.getByLabelText(/prescriber number/i);
+    expect(presNo.value).toBe('');
+  });
+
+  test("Provider data input updates state and therefore it's own value when user types in input", () => {
+    const presNo = screen.getByLabelText(/prescriber number/i);
+    fireEvent.change(presNo, { target: { value: '0123456' } })
+    expect(presNo.value).toBe('0123456');
+  });
+});
+
+describe('Form submit testing (standalone)', () => {
+  // Form submission tests here
+  test('Identifies invalid field on submission attempt (provider section)', () => {
+    const input = screen.getByLabelText(/prescriber number/i);
+    const submit = screen.getByRole('button', { name: 'Save' });
+    fireEvent.change(input, { target: { value: '' } });
+    fireEvent.click(submit);
+    const alert = screen.queryAllByText(/This field cannot be left blank/i);
+    expect(alert.length).toBeGreaterThan(0);
+  });
+
+  test('Valid form does not generate any error alerts on submit', () => {
+    const fullName = screen.getByLabelText(/full name/i)
+    const street = screen.getByLabelText(/street address/i)
+    const suburb = screen.getByLabelText(/suburb/i)
+    const postcode = screen.getByLabelText(/postcode/i)
+    const state = screen.getByLabelText(/state/i)
+    const phone = screen.getByLabelText(/phone number/i)
+    const prescriber = screen.getByLabelText(/prescriber number/i)
+
+    fireEvent.change(fullName, { target: { value: 'Daniel' } });
+    fireEvent.change(street, { target: { value: 'Port Road' } });
+    fireEvent.change(suburb, { target: { value: 'Wynn Vale' } });
+    fireEvent.change(postcode, { target: { value: '5555' } });
+    fireEvent.change(state, { target: { value: 'SA' } });
+    fireEvent.change(phone, { target: { value: '0427779730' } });
+    fireEvent.change(prescriber, { target: { value: '1234567' } });
+
+    const submit = screen.getByRole('button', { name: 'Save' });
+    fireEvent.click(submit);
+
+    const alert = screen.queryByText(/This field cannot be left blank/i);
+    expect(alert).not.toBeInTheDocument();
+  });
+});
+
