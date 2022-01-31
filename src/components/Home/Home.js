6bef126a7ec943574891f96ff561fa7a095c0406
Daniel <daniel.moderiano@outlook.com>
2022-01-31 20:32:19 +1030

    Add title content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import { StyledHome } from "./Home.styled";
 import { Helmet } from 'react-helmet-async';
-import formHalf from '../../assets/4k-form-half.png';
+import formHalf from '../../assets/4k-form-half.jpg';
 import pbsPaper from '../../assets/pbs-paper.jpg';
 import pbsLogo from '../../assets/pbs-logo.svg'
 import { useEffect } from "react";
@@ -15,7 +15,7 @@ const Home = ({ setPage }) => {
   return (<>
     <Helmet>
       <title>Online therapeutic prescriptions for Optometrists | OptomRx</title>
-      <meta name="description" content="Professionally create and manage therapeutic prescriptions online, including PBS and authority scripts"/>
+      <meta name="description" content="Giving Australian optometrists the tools to professionally create and manage therapeutic prescriptions online, including PBS and authority scripts."/>
       <link rel="canonical" href="/dashboard" />
     </Helmet>
 
@@ -25,9 +25,10 @@ const Home = ({ setPage }) => {
       <div className="Home-container">
         <section className="Home__intro">
           <div className="content-container">
-            <h2 className="Home__title">Write computerised therpaeutic prescriptions with <strong>OptomRx</strong></h2>
-            <p className="subtitle">A free-to-use tool giving Australian optometrists the ability to create professional computerised prescriptions for any therapeutic medications, no matter where you're practicing.</p>
+            <h2 className="Home__title">Professional therapeutic prescriptions</h2>
+            <p className="subtitle">OptomRx lets you write therapeutic prescriptions online, with integrated PBS support and tools to supercharge efficiency.</p>
           </div>
+
         </section>
         <section className="Home__preview" aria-labelledby="description__title">
           <div className="content-container preview-container">
@@ -42,18 +43,18 @@ const Home = ({ setPage }) => {
             <div className="description__screenshots">
               {/* Images of desktop and/or mobile here */}
               <img src={formHalf} alt="Screenshot of online form for creating new prescription" />
-              <img src={pbsPaper} alt="Blank PBS computerised prescription paper" />
+              {/* <img src={pbsPaper} alt="Blank PBS computerised prescription paper" /> */}
             </div>
           </div>
         </section>
         <section className="Home__features">
           <div className="content-container">
-            <h3 className="features__title">Key features</h3>
-            <p className="features__subtitle">OptomRx provides a complete suite of tools for all prescriptions</p>
+            <h3 className="features__title">Streamline prescription writing.</h3>
+            <p className="features__subtitle">OptomRx provides unique tools to make writing prescriptions quick and easy.</p>
             <div className="features__cards">
               <div className="features__card">
                 <span className="card-icon"><img src={pbsLogo} alt="PBS logo" /></span>
-                <h4 className="card__title">Real time PBS data</h4>
+                <h4 className="card__title">Real-time PBS data</h4>
                 <p className="card-text">Receive real-time, up-to-date PBS information on your selected medication as you write your prescription.</p>
                 <Button className="card-btn">Learn more</Button>
               </div>
@@ -68,18 +69,18 @@ const Home = ({ setPage }) => {
                 <Button className="card-btn">Learn more</Button>
               </div>
               <div className="features__card">
-                <span className="card-icon"><svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#31776f"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 7v12.97l-4.21-1.81-.79-.34-.79.34L5 19.97V7h10m4-6H8.99C7.89 1 7 1.9 7 3h10c1.1 0 2 .9 2 2v13l2 1V3c0-1.1-.9-2-2-2zm-4 4H5c-1.1 0-2 .9-2 2v16l7-3 7 3V7c0-1.1-.9-2-2-2z"/></svg></span>
-                <h4 className="card__title">Favourite scripts</h4>
-                <p className="card-text">Save your most commonly written prescriptions, and instantly re-prescribe them as needed.</p>
+                <span className="card-icon"> <svg xmlns="http://www.w3.org/2000/svg" className="icon arrow-icon" viewBox="0 0 512 512" width="50px" height="50px"><path d="M448 256L272 88v96C103.57 184 64 304.77 64 424c48.61-62.24 91.6-96 208-96v96z" fill="#31776f" stroke="31776f" strokeLinejoin="round" strokeWidth="10" /></svg></span>
+                <h4 className="card__title">Instant re-prescribe</h4>
+                <p className="card-text">Save your most frequently prescribed medicines as favourites to instantly re-prescribe them with a single click.</p>
                 <Button className="card-btn">Learn more</Button>
               </div>
               <div className="features__card">
                 <span className="card-icon"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="50px" viewBox="0 0 24 24" width="50px" fill="#31776f"><g><rect fill="none" height="24" width="24"/></g><g><g/><g><path d="M16.67,13.13C18.04,14.06,19,15.32,19,17v3h4v-3C23,14.82,19.43,13.53,16.67,13.13z"/><path d="M15,12c2.21,0,4-1.79,4-4c0-2.21-1.79-4-4-4c-0.47,0-0.91,0.1-1.33,0.24C14.5,5.27,15,6.58,15,8s-0.5,2.73-1.33,3.76 C14.09,11.9,14.53,12,15,12z"/><path d="M9,12c2.21,0,4-1.79,4-4c0-2.21-1.79-4-4-4S5,5.79,5,8C5,10.21,6.79,12,9,12z M9,6c1.1,0,2,0.9,2,2c0,1.1-0.9,2-2,2 S7,9.1,7,8C7,6.9,7.9,6,9,6z"/><path d="M9,13c-2.67,0-8,1.34-8,4v3h16v-3C17,14.34,11.67,13,9,13z M15,18H3l0-0.99C3.2,16.29,6.3,15,9,15s5.8,1.29,6,2V18z"/></g></g></svg></span>
                 <h4 className="card__title">Multiple prescribers</h4>
-                <p className="card-text">Add prescriber details for any locations you are practicing, and select between any of them on a script-by-script basis.</p>
+                <p className="card-text">Add prescriber details for all locations you are practicing and seamlessly switch as needed.</p>
                 <Button className="card-btn">Learn more</Button>
               </div>
-              <div className="features__card">
+              {/* <div className="features__card">
                 <span className="card-icon">
                   <svg width="50px" height="50px" viewBox="0 0 24 24">
                     <path fill="#31776f" d="M10 4A4 4 0 0 0 6 8A4 4 0 0 0 10 12A4 4 0 0 0 14 8A4 4 0 0 0 10 4M17.5 13C15 13 13 15 13 17.5C13 20 15 22 17.5 22C20 22 22 20 22 17.5C22 15 20 13 17.5 13M10 14C5.58 14 2 15.79 2 18V20H11.5A6.5 6.5 0 0 1 11 17.5A6.5 6.5 0 0 1 11.95 14.14C11.32 14.06 10.68 14 10 14M17.5 14.5C19.16 14.5 20.5 15.84 20.5 17.5C20.5 18.06 20.35 18.58 20.08 19L16 14.92C16.42 14.65 16.94 14.5 17.5 14.5M14.92 16L19 20.08C18.58 20.35 18.06 20.5 17.5 20.5C15.84 20.5 14.5 19.16 14.5 17.5C14.5 16.94 14.65 16.42 14.92 16Z" />
@@ -88,7 +89,7 @@ const Home = ({ setPage }) => {
                 <h4 className="card__title">No patient data retention</h4>
                 <p className="card-text">For privacy and security reasons, no patient information is kept or stored after a prescription is finished.</p>
                 <Button className="card-btn">Learn more</Button>
-              </div>
+              </div> */}
             </div>
           </div>
           
--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -4,26 +4,24 @@ const StyledHome = styled.main`
   width: 100%;
 
   .content-container {
-    max-width: 1140px;
+    max-width: 1300px;
     margin: 0 1rem;
     text-align: center;
     padding: 1rem;
-    
   }
 
   .Home__title {
-    font-weight: 300;
-    font-size: 2.5rem;
-
-    strong {
-      font-weight: 700;
-    }
+    font-weight: 700;
+    font-size: 4rem;
+    margin: 0 0 2rem 0;
+    line-height: 4.5rem;
   }
 
   .subtitle {
     font-size: 1.3rem;
     font-weight: 600;
     color: #546E7A;
+    max-width: 600px;
   }
 
   .Home__intro {
@@ -37,7 +35,10 @@ const StyledHome = styled.main`
     padding: 1rem 0 5rem 0;
 
     .content-container {
-      max-width: 960px;
+      max-width: 800px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
   }
 
@@ -82,11 +83,11 @@ const StyledHome = styled.main`
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    background-color: #F9FAFB;
+    /* background-color: #F9FAFB; */
 
     .features__title {
       font-size: 2.6rem;
-      margin: 0;
+      margin: 0 0 1rem 0;
     }
 
     .features__subtitle {
@@ -99,11 +100,22 @@ const StyledHome = styled.main`
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
-
+      margin-top: 3rem;
+      width: 100%;
 
       .features__card {
-        max-width: 300px;
+        max-width: 320px;
         margin: 0.75rem;
+        background-color: #fff;
+        /* box-shadow: 0 6.4px 14.4px 0 rgb(0 0 0 / 13%), 0 1.2px 3.6px 0 rgb(0 0 0 / 11%); */
+        border: 1px solid #dee1e4;
+        border-radius: 6px;
+        padding: 2rem 1.5rem;
+
+        &:hover {
+          background-color: #F9FAFB;
+          cursor: pointer;
+        }
 
         h4 {
           margin: 0;
